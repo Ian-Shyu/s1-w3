71c6859bc7b809a8f9795eea14e0af5230fb3920
Ian-S <ianshyu2008@gmail.com>
2026-08-28 03:36:19 +0000

    done

--- ClassroomMain.java
+++ ClassroomMain.java
@@ -1,5 +1,16 @@
 public class ClassroomMain {
     public static void main(String[] args) {
+        Classroom c1 = new Classroom("John", 6, 20);
+        Classroom c2 = new Classroom("Derrek");
+
+        c2.setDesks(30);
+
+        System.out.println("Teacher: " + c1.teacher());
+        System.out.println("Desks: " + c1.students());
+        System.out.println("Students: " + c1.students());
+
+
+
         
     }
 }
